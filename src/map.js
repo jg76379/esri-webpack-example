550d6b8dc669ab479eca0f235c07b1c895454ab2
James Gough <jgough7@gmail.com>
2020-02-11 13:46:41 -0500

    add example feature layer to map

--- src/map.js
+++ src/map.js
@@ -1,5 +1,6 @@
 import MapView from "esri/views/MapView";
 import EsriMap from "esri/Map";
+import FeatureLayer from "esri/layers/FeatureLayer";
 
 export class MainMap {
 
@@ -7,13 +8,24 @@ export class MainMap {
         this.map = new EsriMap({
             basemap: "streets",
             layers: []
-          });
+        });
           
-          this.view = new MapView({
+        this.view = new MapView({
             container: viewContainer,
             map: this.map,
             zoom: 4, 
             center: [15, 65]
-          });
+        });
+
+        // load example feature layer from Arcgis Online
+        const featureLayer = new FeatureLayer({
+            url:
+                "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Major_Cities/FeatureServer/0"
+        });
+        this.map.add(featureLayer);
+
+        featureLayer.when(() => {
+            this.view.goTo({target: featureLayer.fullExtent})
+        })
     }
 }
